47a9d766aa01d0a5bbe947f66738b1550a0ef0c4
private desktop pc <it.s.aemt@gmail.com>
2022-07-09 16:27:10 +0900

    INIT

---
+++ src/main/java/me/snowlight/springcloudorders/SpringCloudOrdersApplication.java
@@ -0,0 +1,13 @@
+package me.snowlight.springcloudorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudOrdersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/snowlight/springcloudorders/SpringCloudOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package me.snowlight.springcloudorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
